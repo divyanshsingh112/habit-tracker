9478e11496427d17b1f542507867ca03c007428d
Divyansh <sdivyansh110205@gmail.com>
2026-01-31 04:57:53 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/habit-tracker/",
+  base: "/habit-tracker/", // <--- CRITICAL: Must match your repo name
+  build: {
+    chunkSizeWarningLimit: 1600, // (Optional) Stops the yellow warning
+  },
 })
